84b3166f201f6199850bbb148de9723ffa3070f6
GBao06 <bunduong12@gmail.com>
2025-07-11 21:12:28 +0700

    sua file

--- js/slider.js
+++ js/slider.js
@@ -1,49 +1,34 @@
-const listImage = document.querySelector('.list-images')
-const imgs = document.getElementsByTagName('img')
-const btnLeft = document.querySelector('.btn-left')
-const btnRight = document.querySelector('.btn-right')
-const length = imgs.length
-let current = 0
+const inner = document.querySelector('.carousel-inner');
+    const totalSlides = document.querySelectorAll('.slide-item').length;
+    const dots = document.querySelectorAll('.dot');
+    let currentIndex = 0;
 
-const handleChangeSlide = () => {
-    if (current == length - 1) {
-        current = 0
-        let width = imgs[0].offsetWidth
-        listImage.style.transform = `translateX(0px)`
-        document.querySelector('.active').classList.remove('active')
-        document.querySelector('.index-item-'+ current).classList.add('active')
-
-    } else {
-        current ++
-        let width = imgs[0].offsetWidth
-        listImage.style.transform = `translateX(${width * -1 * current}px)`
-        document.querySelector('.active').classList.remove('active')
-        document.querySelector('.index-item-'+ current).classList.add('active')
+    function updateSlide() {
+      inner.style.transform = `translateX(-${currentIndex * 100}%)`;
+      dots.forEach(dot => dot.classList.remove('active'));
+      dots[currentIndex].classList.add('active');
     }
-}
 
-let handleEventChangeSlide = setInterval(handleChangeSlide, 3000)
+    document.querySelector('.btn--right').addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % totalSlides;
+      updateSlide();
+    });
 
-btnRight.addEventListener('click', () => {
-    clearInterval(handleEventChangeSlide)
-    handleChangeSlide()
-    handleEventChangeSlide = setInterval(handleChangeSlide, 3000)
-})
+    document.querySelector('.btn--left').addEventListener('click', () => {
+      currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+      updateSlide();
+    });
 
-btnLeft.addEventListener('click', () => {
-    clearInterval(handleEventChangeSlide)
-    if (current == 0) {
-        current = length - 1
-        let width = imgs[0].offsetWidth
-        listImage.style.transform = `translateX(${width * -1 * current}px)`
-        document.querySelector('.active').classList.remove('active')
-        document.querySelector('.index-item-'+ current).classList.add('active')
-    } else {
-        current --
-        let width = imgs[0].offsetWidth
-        listImage.style.transform = `translateX(${width * -1 * current}px)`
-        document.querySelector('.active').classList.remove('active')
-        document.querySelector('.index-item-'+ current).classList.add('active')
-    }
-    handleEventChangeSlide = setInterval(handleChangeSlide, 4000)
-})
+    dots.forEach(dot => {
+      dot.addEventListener('click', () => {
+        currentIndex = +dot.dataset.index;
+        updateSlide();
+      });
+    });
+
+    setInterval(() => {
+      currentIndex = (currentIndex + 1) % totalSlides;
+      updateSlide();
+    }, 5000);
+
+   updateSlide(); 
