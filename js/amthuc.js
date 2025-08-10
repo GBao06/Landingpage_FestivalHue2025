4d253e618778714e875fbd3db52a4c7569b96633
tp2405wind <vtn2405hanlam@gmail.com>
2025-08-10 17:45:52 +0700

    Cập nhật nhiều file và thêm các module mới

---
+++ js/amthuc.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btnExplore = document.querySelector(".food-epl");
+  const btnBack = document.querySelector(".btn-back");
+  const wrapperFood = document.querySelector(".wrapper-food");
+  const wrapperAfter = document.querySelector(".wrapper-food-after");
+  const navItems = document.querySelectorAll(".nav-item");
+
+  btnExplore.addEventListener("click", function () {
+    wrapperFood.classList.add("hidden");
+    wrapperAfter.classList.remove("hidden");
+
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    wrapperAfter.classList.add("active");
+
+    navItems.forEach((item, index) => {
+      setTimeout(() => {
+        item.classList.add("show");
+      }, index * 300);
+    });
+
+    wrapperAfter.scrollIntoView({ behavior: "smooth" });
+  });
+
+  btnBack.addEventListener("click", function () {
+    wrapperAfter.classList.add("hidden");
+    wrapperFood.classList.remove("hidden");
+
+    navItems.forEach((item) => item.classList.remove("show"));
+  });
+});
